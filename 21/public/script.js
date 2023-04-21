0acf94690b0443268e53cc7c62d58c91f326c237
lera buranko <76012407+ppnnh@users.noreply.github.com>
2023-04-21 10:11:24 +0300

    Update script.js

--- 21/public/script.js
+++ 21/public/script.js
@@ -1,24 +1,11 @@
-async function Update(){
-    const FIO=document.getElementById("FIO").value
-    const number=document.getElementById("number").value
-    let obj={
-        FIO: FIO,
-        number: number
-    }
-    console.log(obj)
-    const response=await fetch(`/update`,{
-        method:'POST',
-        headers: {"Content-Type":"application/json"},
-        body: JSON.stringify(obj)
-    });
-    console.log(JSON.stringify(obj))
-}
+
 async function Delete(){ 
     const id=document.getElementById("FIO").value
     console.log(id)
     const response=await fetch(`/delete/${id}`,{
         method:'POST'
     });
+    window.location.href='/'
 }
 function Change() {
     document.getElementById("Delete").disabled = true;
